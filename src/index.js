b85cad20a0846c6b9d133655b5b4e0a33662a4d6
Scott Le <scottle220044@gmail.com>
2026-01-19 17:02:53 -0800

    added sidebar and blank modules

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import './style.css';
+
+import { Sidebar } from './sidebar.js';
+import { Todo } from './todo.js';
+
 console.log("Working")
+
+const content = document.getElementById("content");
+const header = document.createElement("h1");
+header.textContent = "To Do List";
+
+content.appendChild(header);
+
+const mySidebar = Sidebar();
+
+const mytodo = Todo("First to do", "Check out my first description", "goal #1", "wednesday", "Top");
+// console.log(mytodo.title);
+// console.log(mytodo.dueDate);
+
+//const myDomController = domController();
+
---
+++ src/todo.js
@@ -0,0 +1,16 @@
+export const Todo = (title, description, dueDate, priority, notes) =>{
+
+    let completed = false;
+    const completeToggle = () => {
+        completed = ! completed;
+    } 
+
+    const getStatus = () => {
+        return completed;
+    }
+
+    return {
+        title, description, dueDate, priority, notes, getStatus, completeToggle
+    }
+    
+} 
